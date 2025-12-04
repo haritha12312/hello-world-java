f6e7c6fe7765c3ff2cb94379e0aa8c8c0c662ea6
haritha12312 <harithapenubadi@gmail.com>
2025-12-04 21:41:40 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaDevOpsss");
      System.out.println("Hello, devopsbbykrishna1");
-     System.out.println("Hello, haritha01"); 
+     System.out.println("Hello, haritha02"); 
 
   }
 }
